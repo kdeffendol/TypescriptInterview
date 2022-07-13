6936297f3f9743fcde5fdb971ed7067818596a6f
kdeffendol <kelsey.deffendol@centeva.com>
2022-07-13 16:17:33 -0600

    Add customer table and table sorting

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CustomerRecord, CUSTOMER_RECORDS } from 'src/app/constants/customer-records';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements AfterViewInit {
 
   public records: CustomerRecord[] = CUSTOMER_RECORDS;
+  public recordsTableData = new MatTableDataSource(this.records);
+
+  public readonly columnsToDisplay = [
+    'FirstName',
+    'LastName',
+    'Age',
+    'ViewDetails'
+  ]
   
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  @ViewChild(MatSort) sort: MatSort = new MatSort;
 
+  ngAfterViewInit(): void {
+    this.recordsTableData.sort = this.sort;
+  }
 }
--- src/app/components/table/table.module.ts
+++ src/app/components/table/table.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table.component';
+import { MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
 
 
 
@@ -9,7 +12,10 @@ import { TableComponent } from './table.component';
     TableComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSortModule
   ],
   exports: [
     TableComponent
